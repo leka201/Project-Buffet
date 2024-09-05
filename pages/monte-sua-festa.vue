2162d56dcc2d31b7dfdfce19cbe29f15ad46c5e6
leka201 <alexiaizabelasantos@hotmail.com>
2024-09-05 17:15:34 -0300

    up monte sua festa

---
+++ pages/monte-sua-festa.vue
@@ -0,0 +1,71 @@
+<template>
+    <h1><strong>Monte seu evento:</strong></h1>
+
+<p>1. Tipo de festa</p>
+
+ <p>Filtros:</p>
+ <input type="radio" name="objetivo"/>Festa Infantil
+ <input type="radio" name="objetivo"/>Festa Adolescente
+ <input type="radio" name="objetivo"/>Festa Adulto
+
+<p>2.Decoração</p>
+
+ <p>Filtros:</p>
+ <input type="radio" name="objetivo"/>Futebol
+ <input type="radio" name="objetivo"/>#####
+
+ <p>3.Itens para a festa</p>
+
+ <p>Filtros:</p>
+ <input type="radio" name="objetivo"/>Cadeiras e Mesas
+ <input type="radio" name="objetivo"/>#####
+
+
+<p>4.Comida</p>
+<p>Filtros:</p>
+ <input type="radio" name="objetivo"/>Salgados
+ <input type="radio" name="objetivo"/>Refrigerantes
+ <input type="radio" name="objetivo"/>Bolos
+
+ <p>5.Resumo</p>
+
+ <p>Itens selecionados:</p>
+ <p>Tipo de festa:</p>
+ <p>Festa Infantil</p>
+
+ <p>Decoração:</p>
+ <p>Futebol</p>
+
+ <p>Itens para a festa:</p>
+ <p>Cadeiras e Mesas</p>
+ 
+ <p>Comida:</p>
+ <p>Salgados</p>
+ <p>Refrigerantes</p>
+
+ <div class="menu">
+    <img src="https://placeholder.co/70">
+    
+</div>
+
+<h1><strong> Monte seu evento: </strong></h1>
+
+<p>1. Tipo de festa</p>
+
+<p>Filtros:</p>
+<input type="radio" name="objetivo"/>Festa Infantil
+<input type="radio" name="objetivo"/>Festa Adolescente
+<input type="radio" name="objetivo"/>Festa Adulto
+
+<p>2.Decoração</p>
+
+<p>Filtros:</p>
+<input type="radio" name="objetivo"/>Futebol
+<input type="radio" name="objetivo"/>#####
+
+<p>3.Itens para a festa</p>
+
+<p>Filtros:</p>
+<input type="radio" name="objetivo"/>Cadeiras e Mesas
+<input type="radio" name="objetivo"/>#####
+</template>
