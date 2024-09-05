c22dc51ed8cf573846e3d79e35c88e73ba6bfa10
MateusBahia <mateusmmalves2@gmail.com>
2024-09-05 17:24:47 -0300

    minha parte ta ai

--- app.vue
+++ app.vue
@@ -8,4 +8,8 @@
   <NuxtLink to="/pagamento">Vá pagar</NuxtLink>
 
   <NuxtLink to="/pagamento_aprovado">Deu certo</NuxtLink>
+
+  <NuxtLink to="/cadastro">cadastro</NuxtLink>
+
+  <NuxtLink to="/login">login</NuxtLink>
 </template>
