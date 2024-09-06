3ebfe90e01ecefc88601cdd1b04697eee1bb013a
Vitor Amaro <vitoramarogit@gmail.com>
2024-09-06 14:10:50 -0300

    menu

---
+++ components/Menu.vue
@@ -0,0 +1,13 @@
+<template>
+
+<div class="bordaredonda">
+
+    <span class="fundoroxo "> <i class="fa-solid fa-location-dot distpalavras ">   </i> <spam class="dist1">R. Episcopal, 700 - Centro, São Carlos - SP, 13560-570   </spam>   
+        
+                             <i class="fa-solid fa-phone dist2 "></i> <span class="dist1"> +55 16 992448042 </span>
+                             <i class="fa-solid fa-envelope dist2"></i> <span class="dist1"> mateusbahia@gmail.com</span> 
+              
+                            </span>  
+</div>
+
+</template>
