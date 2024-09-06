ccd1427fabb8818158fa991f87e5006e961022a4
Vitor Amaro <vitoramarogit@gmail.com>
2024-09-06 17:11:36 -0300

    mudanças cabulosas

--- app.vue
+++ app.vue
@@ -1,23 +1,18 @@
+<script>
+
+import('~/assets/css/index.css')
+
+</script>
+
+
 <template>
+  
+
+
+
+  <Menu/>
   <NuxtPage />
-  <NuxtLink to="/">Inicio</NuxtLink>
   
-  <NuxtLink to="/perfil">Entre no seu perfil</NuxtLink>
-
-  <NuxtLink to="/carrinho">Veja seu carrinho</NuxtLink>
-
-  <NuxtLink to="/pagamento">Vá pagar</NuxtLink>
-
-  <NuxtLink to="/pagamento_aprovado">Deu certo</NuxtLink>
-
-  <NuxtLink to="/cadastro">cadastro</NuxtLink>
-
-  <NuxtLink to="/login">login</NuxtLink>
-  
-
-<NuxtLink to="/monte-sua-festa">monte o seu</NuxtLink>
-
-<NuxtLink to="/recuperar-senha">recuperar sua senha</NuxtLink>
 
 
 </template>
