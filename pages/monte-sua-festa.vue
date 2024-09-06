59fa0b0eaec1ea8b46623253b1d8a13517124b7c
leka201 <alexiaizabelasantos@hotmail.com>
2024-09-06 17:28:54 -0300

    design do site quase pronto e adição de elementos vue/nuxt

--- pages/monte-sua-festa.vue
+++ pages/monte-sua-festa.vue
@@ -1,73 +1,121 @@
+<script setup>
+
+import ('assets/css/monte-sua-festa.css')
+
+let lista = ref("tipo_festa")
+
+function tipo_festa(){
+
+    lista.value = "tipo_festa";
+
+}
+
+function decoracao(){
+
+    lista.value = "decoracao";
+
+}
+
+function itens_festa(){
+
+    lista.value = "itens_festa";
+
+}
+
+function comida(){
+
+    lista.value = "comida";
+
+}
+
+</script>
+
 <template>
-    <h1><strong>Monte seu evento:</strong></h1>
 
-<p>1. Tipo de festa</p>
+    <div class="container">
 
- <p>Filtros:</p>
- <input type="radio" name="objetivo"/>Festa Infantil
- <input type="radio" name="objetivo"/>Festa Adolescente
- <input type="radio" name="objetivo"/>Festa Adulto
+            <div>
+                <ul>Monte seu evento:
 
-<p>2.Decoração</p>
-
- <p>Filtros:</p>
- <input type="radio" name="objetivo"/>Futebol
- <input type="radio" name="objetivo"/>#####
-
- <p>3.Itens para a festa</p>
-
- <p>Filtros:</p>
- <input type="radio" name="objetivo"/>Cadeiras e Mesas
- <input type="radio" name="objetivo"/>#####
+                    <ul class="filha"><button v-on:click="tipo_festa()">Tipo de festa</button></ul> 
+                    <hr> 
+                    <ul class="filha"><button v-on:click="decoracao()">Decoração</button></ul>
+                    <hr> 
+                    <ul class="filha"><button v-on:click="itens_festa()">Itens para a festa</button></ul>
+                    <hr> 
+                    <ul class="filha"><button v-on:click="comida()">Comida</button></ul> 
+                            
+                </ul>
+            </div>
 
 
-<p>4.Comida</p>
-<p>Filtros:</p>
- <input type="radio" name="objetivo"/>Salgados
- <input type="radio" name="objetivo"/>Refrigerantes
- <input type="radio" name="objetivo"/>Bolos
+            <div class="produtos">
 
- <p>5.Resumo</p>
+        <div v-if=" lista == 'tipo_festa'">
+            <p>Filtros:</p>
+                <input type="radio" name="objetivo"/>Festa Infantil
+                <input type="radio" name="objetivo"/>Festa Adolescente
+                <input type="radio" name="objetivo"/>Festa Adulto
 
- <p>Itens selecionados:</p>
- <p>Tipo de festa:</p>
- <p>Festa Infantil</p>
+        </div>
 
- <p>Decoração:</p>
- <p>Futebol</p>
+        <div v-if=" lista == 'decoracao'">
 
- <p>Itens para a festa:</p>
- <p>Cadeiras e Mesas</p>
- 
- <p>Comida:</p>
- <p>Salgados</p>
- <p>Refrigerantes</p>
+            <p>Filtros:</p>
+                <input type="radio" name="objetivo"/>Futebol
+                <input type="radio" name="objetivo"/>#####
 
- <div class="menu">
-    <img src="https://placeholder.co/70">
-    
-</div>
+        </div>
 
-<h1><strong> Monte seu evento: </strong></h1>
+        <div v-if=" lista == 'itens_festa'">
 
-<p>1. Tipo de festa</p>
+            <p>Filtros:</p>
+                <input type="radio" name="objetivo"/>Cadeiras e Mesas
+                <input type="radio" name="objetivo"/>#####
 
-<p>Filtros:</p>
-<input type="radio" name="objetivo"/>Festa Infantil
-<input type="radio" name="objetivo"/>Festa Adolescente
-<input type="radio" name="objetivo"/>Festa Adulto
+        </div>
 
-<p>2.Decoração</p>
+        <div v-if=" lista == 'comida'">
 
-<p>Filtros:</p>
-<input type="radio" name="objetivo"/>Futebol
-<input type="radio" name="objetivo"/>#####
+            <p>Filtros:</p>
+                <input type="radio" name="objetivo"/>Salgados
+                <input type="radio" name="objetivo"/>Refrigerantes
+                <input type="radio" name="objetivo"/>Bolos
 
-<p>3.Itens para a festa</p>
+        </div>
 
-<p>Filtros:</p>
-<input type="radio" name="objetivo"/>Cadeiras e Mesas
-<input type="radio" name="objetivo"/>#####
+                <!-- <p>
+                    Itens selecionados:
+                    <br>
+                    <br>
+                    Tipo de festa:
+                    <br>
+                    <br>
+                    Festa Infantil 
+                </p>
+
+                <p>
+                    Decoração:
+                    <br>
+                    Futebol
+                </p>
+
+                <p>
+                    Itens para a festa:
+                    <br>
+                    Cadeiras e Mesas
+                </p>
+                
+                <p>
+                    Comida:
+                    <br>
+                    Salgados
+                    <br>
+                    Refrigerantes
+                </p> -->
+            </div>
+
+    </div>
 
 </template>
 
