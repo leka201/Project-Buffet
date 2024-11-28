9b0e54c116bc774bd2f8941a99d1cdbdc7b6f071
leka201 <alexiaizabelasantos@hotmail.com>
2024-11-28 17:21:46 -0300

    ter q recriar o monte sua festa

--- pages/monte-sua-festa.vue
+++ pages/monte-sua-festa.vue
@@ -10,18 +10,20 @@
     async function BuscarProdutos(){
 
         const resposta = await axios.get("http://localhost:3001/item/read")
-        
+        console.log("item")
+        console.log(resposta.data.db)
         produtos.value = resposta.data.db
         console.log(produtos.value )
     }
     onMounted ( () => {
+        console.log("entrou")
         BuscarProdutos()
     })
 
     // Sugestão de estudo: funções filter, forEach e map
 
-    let lista = ref("decoracao")
-    let produtos_festa = ref("decoracao")
+    let lista = ref("decorations")
+    let produtos_festa = ref("decorations")
     let produts =ref("false")
     let filtro = ref("")
     console.log(filtro)
@@ -30,44 +32,44 @@
         filtro.value = valor;
     }
 
-    const filtros_decoracao = reactive([
-    {nome:"Decoração de Futebol", filtro:"decoração_fut"},
-    {nome:"Decoração de Casamento com Flores", filtro:"decoração_casam"}
+    const filtros_decorations = reactive([
+    {name:"Decoração de Futebol"},
+    {name:"Decoração de Casamento com Flores"}
     ])
 
-    const filtros_itens_festa = reactive([
-    {nome:"Mesas", filtro:"item_mesas"},
-    {nome:"Toalhas", filtro:"item_toalhas"},
-    {nome:"Pratos", filtro:"item_pratos"},
-    {nome:"Talheres", filtro:"item_talheres"},
-    {nome:"Cadeiras", filtro:"item_cadeiras"}
+    const filtros_items = reactive([
+    {name:"Mesas", filtro:"item_mesas"},
+    {name:"Toalhas", filtro:"item_toalhas"},
+    {name:"Pratos", filtro:"item_pratos"},
+    {name:"Talheres", filtro:"item_talheres"},
+    {name:"Cadeiras", filtro:"item_cadeiras"}
     ])
     
-    const filtros_comida = reactive([
-    {nome:"Salgados", filtro:"comida_salgados"},
-    {nome:"Doces", filtro:"comida_doces"},
-    {nome:"Bebidas", filtro:"comida_bebidas"},
+    const filtros_food = reactive([
+    {name:"Salgados", filtro:"food_salgados"},
+    {name:"Doces", filtro:"food_doces"},
+    {name:"Bebidas", filtro:"food_bebidas"},
     ])
 
-    function decoracao(){
+    function decorations(){
 
-        lista.value = "decoracao";
-        produtos_festa.value = "decoracao";
+        lista.value = "decorations";
+        produtos_festa.value = "decorations";
         produts = true;
 
     }
 
-    function itens_festa(){
+    function items(){
 
-        produtos_festa.value = "decoracao";
-        lista.value = "itens_festa";
+        produtos_festa.value = "decorations";
+        lista.value = "items";
 
     }
 
-    function comida(){
+    function food(){
 
-        lista.value = "comida";
-        produtos_festa.value = "decoracao";
+        lista.value = "food";
+        produtos_festa.value = "decorations";
 
     }
 
@@ -80,59 +82,59 @@
             <div class="lateral">
                 <ul>Monte seu evento:
 
-                    <ul class="filha"><button v-on:click="decoracao()">Decoração</button></ul>
+                    <ul class="filha"><button v-on:click="decorations()">Decoração</button></ul>
                     <hr> 
-                    <ul class="filha"><button v-on:click="itens_festa()">Itens para a festa</button></ul>
+                    <ul class="filha"><button v-on:click="items()">Itens para a festa</button></ul>
                     <hr> 
-                    <ul class="filha"><button v-on:click="comida()">Comida</button></ul> 
+                    <ul class="filha"><button v-on:click="food()">Comida</button></ul> 
                             
                 </ul>
             </div>
 
             <div class="produtos">
 
-                <div class=" animate__animated animate__fadeInLeft animate__faster espaco" v-if=" lista == 'decoracao'">
+                <div class=" animate__animated animate__fadeInLeft animate__faster espaco" v-if=" lista == 'decorations'">
 
                     <p>Filtros:</p>
                     <div class="flex_filtros">
-                    <p v-for="filtro_decoracao in filtros_decoracao">
-                       <input type="radio" name="objetivo" v-on:change="alteraFiltro(filtro_decoracao.filtro)" />{{ filtro_decoracao.nome }}
+                    <p v-for="filtro_decorations in filtros_decorations">
+                       <input type="radio" name="objetivo" v-on:change="alteraFiltro(filtro_decorations.filtro)" />{{ filtro_decorations.name }}
                     </p>
                     </div>
                         
                         <div class="flex">
 
                             <div v-for="produto in produtos.value">                                     
-                                <Mesa class="flex" v-bind:adicionaAoCarrinho="adicionaAoCarrinho"  v-if="filtro == produto.tipo || filtro == ''" v-bind:produtos="produto"/>                      
+                                <Mesa class="flex" v-bind:adicionaAoCarrinho="adicionaAoCarrinho"  v-if="filtro == produto.decorations || filtro == ''" v-bind:produtos="produto"/>                      
                             </div>                            
                         </div>
                 </div>
                 
-                <div class=" animate__animated animate__fadeInLeft" v-if=" lista == 'itens_festa'">
+                <div class=" animate__animated animate__fadeInLeft" v-if=" lista == 'items'">
 
                     <p>Filtros:</p>
                     <div class="flex_filtros">
-                    <p v-for="filtro_item in filtros_itens_festa">
-                       <input type="radio" name="objetivo" v-on:change="alteraFiltro(filtro_item.filtro)"/>{{ filtro_item.nome }}
+                    <p v-for="filtro_item in filtros_items">
+                       <input type="radio" name="objetivo" v-on:change="alteraFiltro(filtro_item.filtro)"/>{{ filtro_item.name }}
                     </p>
                     </div>
 
                         <div class="flex">
 
                             <div v-for="produto in produtos">
-                                <Mesa class="flex"  v-if="filtro == produto.tipo || filtro == ''" v-bind:produtos="produto"/>
+                                <Mesa class="flex"  v-if="filtro == produto.decorations || filtro == ''" v-bind:produtos="produto"/>
                                 
                             </div>
                     
                         </div>
                 </div>
 
-                <div class="animate__animated animate__fadeInLeft" v-if=" lista == 'comida'">
+                <div class="animate__animated animate__fadeInLeft" v-if=" lista == 'food'">
 
                     <p>Filtros:</p>
                     <div class="flex_filtros">
-                    <p v-for="filtros_comida in filtros_comida">
-                       <input type="radio" name="objetivo" v-on:change="alteraFiltro(filtros_comida.filtro)"/>{{ filtros_comida.nome }}
+                    <p v-for="filtros_food in filtros_food">
+                       <input type="radio" name="objetivo" v-on:change="alteraFiltro(filtros_food.filtro)"/>{{ filtros_food.name }}
                     </p>
                     </div>
                     
@@ -140,7 +142,7 @@
 
                         <div v-for="produto in produtos">
                                 
-                            <Mesa class="flex"  v-if="filtro == produto.tipo || filtro == ''" v-bind:produtos="produto"/>
+                            <Mesa class="flex"  v-if="filtro == produto.decorations || filtro == ''" v-bind:produtos="produto"/>
                                 
                         </div>
 
